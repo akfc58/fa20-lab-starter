cfb89fdca7fcc265ca3bd76c56ef1902152bef95
uuuate <uuuate@gmail.com>
2023-11-05 15:30:25 +0800

    done with lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,16 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head, *hare = head;
+    if (head == NULL){
+        return 0;
+    }
+    while (hare->next != NULL && hare->next->next != NULL) {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
     return 0;
 }
--- lab01/segfault_ex.c
+++ lab01/segfault_ex.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 int main() {
     int a[20];
     for (int i = 0; ; i++) {
         a[i] = i;
+        printf("%d\n",a[i]);
     }
 }
